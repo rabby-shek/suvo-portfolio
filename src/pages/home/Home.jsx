864babd5aac1099ce573c5681d26324084065f61
rabby-shek <rabbyshek1998@gmail.com>
2024-06-04 21:59:06 +0600

    new add :: a new txt file for saving for storing codes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,10 +4,12 @@ import Engineering from '../../components/home/Engineering';
 import Experience from '../../components/home/Experience';
 import Article from '../../components/home/Article';
 import Contact from '../../components/home/Contact';
+import Header from '../../components/header/Header';
 
 const Home = () => {
   return (
     <div className='snap'>
+      <Header />
       <HeroSection />
       <Engineering />
       <Experience />
