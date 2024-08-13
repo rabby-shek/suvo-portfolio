f53248c901f66995e9bb7fe3681ad51213935c2a
rabby-shek <rabbyshek1998@gmail.com>
2024-08-13 12:43:31 +0600

    new data structure certificate added

--- src/components/home/Certificates.jsx
+++ src/components/home/Certificates.jsx
@@ -1,58 +1,69 @@
 import React from "react";
 import SoloLearnCSSCertificate from "../../assets/images/css-solo-learn-cirtificate.png";
-import SoloLearnJSCertificate from '../../assets/images/solo-l-js.png';
-import SoloLearnResponsiveCertificate from '../../assets/images/solo-learn-responsive.png';
-import IntroAlgorithmCertificate from '../../assets/images/introduction-algorithm-data-structure.png';
-import GoogleCertificate1 from '../../assets/images/google-colab-1.PNG';
-import GoogleCertificate2 from '../../assets/images/Gemini-api-program.PNG';
+import SoloLearnJSCertificate from "../../assets/images/solo-l-js.png";
+import SoloLearnResponsiveCertificate from "../../assets/images/solo-learn-responsive.png";
+import IntroAlgorithmCertificate from "../../assets/images/introduction-algorithm-data-structure.png";
+import GoogleCertificate1 from "../../assets/images/google-colab-1.PNG";
+import GoogleCertificate2 from "../../assets/images/Gemini-api-program.PNG";
 import HackerRankJS from "../../assets/images/hacker-rank-js-cirtificate.jpg";
+import MasterLinearDataStructure from "../../assets/images/master-linear-data-structure.jpg";
 const Certificates = () => {
   const certificateList = [
     {
       id: 1,
       path: SoloLearnCSSCertificate,
-      flip: 'flip-right'
+      flip: "flip-right",
     },
     {
       id: 2,
       path: SoloLearnJSCertificate,
-      flip: 'flip-left'
+      flip: "flip-left",
     },
     {
       id: 3,
       path: SoloLearnResponsiveCertificate,
-      flip: 'flip-right'
+      flip: "flip-right",
     },
     {
       id: 4,
       path: IntroAlgorithmCertificate,
-      flip: 'flip-left'
+      flip: "flip-left",
     },
     {
       id: 5,
       path: GoogleCertificate1,
-      flip: 'flip-right'
+      flip: "flip-right",
     },
     {
       id: 6,
       path: GoogleCertificate2,
-      flip: 'flip-left'
+      flip: "flip-left",
     },
     {
       id: 7,
       path: HackerRankJS,
-      flip: 'flip-right'
-    }
-    
+      flip: "flip-right",
+    },
+    {
+      id: 8,
+      path: MasterLinearDataStructure,
+      flip: "fli-left",
+    },
   ];
   return (
     <section className="certificate-container">
       <div className="container">
-        <div className="title" data-aos="fade-up">Certificates</div>
+        <div className="title" data-aos="fade-up">
+          Certificates
+        </div>
         <div className="row g-3">
           {certificateList.map((certificate) => {
             return (
-              <div key={certificate.id} className="col-lg-6 col-sm-6" data-aos={certificate.flip}>
+              <div
+                key={certificate.id}
+                className="col-lg-6 col-sm-6"
+                data-aos={certificate.flip}
+              >
                 <img src={certificate.path} alt="certificate" />
               </div>
             );
